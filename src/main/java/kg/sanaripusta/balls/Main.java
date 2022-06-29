0bf5f194bf9e6c20e7fbae1b903b9cc92be981c7
Azamat <abeksadaev@gmail.com>
2022-06-29 14:37:56 +0600

    Change .gitignore

--- src/main/java/kg/sanaripusta/balls/Main.java
+++ src/main/java/kg/sanaripusta/balls/Main.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("hello-view.fxml"));
-//        String cssPath = Objects.requireNonNull(this.getClass().getResource("hello-view.fxml")).toExternalForm();
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("main-view.fxml"));
+//        String cssPath = Objects.requireNonNull(this.getClass().getResource("main-view.fxml")).toExternalForm();
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Balls");
         stage.setScene(scene);
